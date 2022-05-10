8e5c2d5010ace1e93924c56c8beb5b021b12ec46
mr-bobur <bmatyoquboff@gmail.com>
2022-05-10 15:32:26 +0500

    chage db conf

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,12 +2,13 @@ module.exports = {
   HOST: "http://185.196.214.199:3306",
   USER: "root",
   PASSWORD: "acdb2022",
+
   DB: "testdb",
   dialect: "mysql",
   pool: {
     max: 5,
     min: 0,
-    acquire: 30000,
+    acquire: 20000,
     idle: 10000
   }
 };
