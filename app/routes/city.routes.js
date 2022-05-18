a17bca7d1eee82c89fa28ec17f252308acc6252c
mr-bobur <bmatyoquboff@gmail.com>
2022-05-18 16:07:41 +0500

    asd

--- app/routes/city.routes.js
+++ app/routes/city.routes.js
@@ -8,7 +8,7 @@
   router.post("/", [authJwt.verifyToken,authJwt.isAdmin],city.create);
   router.get("/",[authJwt.verifyToken], city.findAll);
   router.get("/:id",[authJwt.verifyToken],  city.findOne);
-  router.get("/:id/devices",[authJwt.verifyToken,authJwt.isAdmin], city.findOneWithDevice);
+  router.get("/:id/devices",[authJwt.verifyToken], city.findOneWithDevice);
   router.get("/:id/users", [authJwt.verifyToken], city.findUsers);
   router.post("/adduser",[authJwt.verifyToken,authJwt.isAdmin], city.addUser);
   router.post("/deleteuser",[authJwt.verifyToken,authJwt.isAdmin], city.deleteUser);
