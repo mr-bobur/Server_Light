b064c7a27803dd1a13c295ba98ff5e78244f1dc1
mr-bobur <bmatyoquboff@gmail.com>
2022-06-18 21:00:04 +0500

    log

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -42,10 +42,11 @@ exports.signup = (req, res) => {
 };
 
 exports.signin = (req, res) => {
-    User.findOne({
+    User.findOne(
+        {
         where: {
             username: req.body.username
-        }},  { include: { all: true }}
+        }}, { include:  { all: true }}
         ).then(user => {
             if (!user) {
                 return res.status(404).send({ message: "User Not found." });
@@ -76,15 +77,16 @@ exports.signin = (req, res) => {
                     authorities.push("ROLE_" + roles[i].name.toUpperCase());
                 }
                 // console.log(user);
-
-                res.status(200).send({
-                    id: user.id,
-                    username: user.username,
-                    email: user.email,
-                    roles: authorities,
-                    accessToken: token, 
-                    cities: user.cities
-                });
+                
+                    res.status(200).send({
+                        id: user.id,
+                        username: user.username,
+                        email: user.email,
+                        roles: authorities,
+                        accessToken: token, 
+                        cities: user.cities
+                    });
+                
             });
         })
         .catch(err => {
